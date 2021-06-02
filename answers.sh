277004c2a580d4963c1b66bbe837f38629704627
Lara-Jalloul <laraadjalloul@gmail.com>
2021-06-02 21:13:33 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+select Name from students
+select * from students WHERE Age>30
+select Name from students where Gender='F' and Age='30'
+select Points from students where Name='Alex'
+insert into students values ('7', 'Lara', '22', 'F', '500')
+update students SET Points =  Points + 5 where Name='Basma'
+update students set Points = Points -5 where Name='Alex'
