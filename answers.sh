d17fd91d152aedd373baaf2b6e133484b69df5e6
Lara-Jalloul <laraadjalloul@gmail.com>
2021-06-03 11:31:19 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,9 @@ select Points from students where Name='Alex'
 insert into students values ('7', 'Lara', '22', 'F', '500')
 update students SET Points =  Points + 5 where Name='Basma'
 update students set Points = Points -5 where Name='Alex'
+
+Creating Table:
+
+ 1. INSERT INTO graduates (Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM students where Name='Layal'
+ 2. update graduates set Graduation='08/09/2018' where Name='Layal'
+ 3. delete from students where Name='Layal'
